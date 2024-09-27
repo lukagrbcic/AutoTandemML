311bd7364957fa9fdf9ef083804629cfa895d5ee
lukagrbcic <luka.grbcic@gmail.com>
2024-09-27 13:37:13 -0700

    testing benchmarks

--- benchmark_files/friedman_multioutput.py
+++ benchmark_files/friedman_multioutput.py
@@ -1,7 +1,8 @@
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import matplotlib.pyplot as plt
 
 np.random.seed(4)
@@ -28,11 +29,17 @@ def friedman_multioutput(n_samples, n_targets, noise_level=0.1):
     
     for i in range(n_targets):
         # Modify coefficients for each target
-        a = np.random.uniform(5, 10)  # Instead of (0, 10)
-        b = np.random.uniform(25, 35)  # Instead of (20, 40)
-        c = np.random.uniform(0.4, 0.6)  # Instead of (0, 1)
+        # a = np.random.uniform(5, 10)  # Instead of (0, 10)
+        # b = np.random.uniform(25, 35)  # Instead of (20, 40)
+        # c = np.random.uniform(0.4, 0.6)  # Instead of (0, 1)
+        # d = np.random.uniform(2, 3)  # Instead of (1, 4)
+        # e = np.random.uniform(0.4, 0.6)  # Instead of (0, 1)
+        
+        a = np.random.uniform(5, 8)  # Instead of (0, 10)
+        b = np.random.uniform(25, 28)  # Instead of (20, 40)
+        c = np.random.uniform(0.4, 0.5)  # Instead of (0, 1)
         d = np.random.uniform(2, 3)  # Instead of (1, 4)
-        e = np.random.uniform(0.4, 0.6)  # Instead of (0, 1)
+        e = np.random.uniform(0.4, 0.5)  # Instead of (0, 1)
 
         
         # Calculate target using modified Friedman #1 function
@@ -47,11 +54,15 @@ def friedman_multioutput(n_samples, n_targets, noise_level=0.1):
 
 # Usage example
 n_samples = 100000
-n_targets = 5
-X, y = friedman_multioutput(n_samples, n_targets)
+n_targets = 10
+y, X = friedman_multioutput(n_samples, n_targets)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, random_state=23)
 X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, shuffle=True, random_state=23)
+
+# X_train, X_test, y_train, y_test = train_test_split(y, X, test_size=0.3, shuffle=True, random_state=23)
+# X_train_, X_val, y_train_, y_val = train_test_split(y_train, X_train, test_size=0.1, shuffle=True, random_state=23)
+
 
 
 # Create DMatrix objects for XGBoost
@@ -83,13 +94,16 @@ model = xgb.train(
 )
 
 
-def calculate_rmse(y_true, y_pred):
-    # print (len(y_true))
-    
+def calculate_rmse(y_true, y_pred):   
     rmse = np.mean(np.array([np.sqrt(mean_squared_error(y_true[i], y_pred[i])) for i in range(len(y_pred))]))
     return rmse
-    # return np.sqrt(np.mean((y_true - y_pred)**2, axis=0))
-    # return np.mean([np.sqrt(mean_squared_error(y_true[i], y_pred[i]) for i in range(len(y_true)))])
+
+
+def calculate_mape(y_true, y_pred):   
+    mape = np.mean(np.array([mean_absolute_percentage_error(y_true[i], y_pred[i]) for i in range(len(y_pred))]))
+    return mape
+
+
 
 # Evaluate on validation set
 y_val_pred = model.predict(dval)
@@ -104,11 +118,19 @@ print("\nTest RMSE per target:", test_rmse_per_target)
 print("Average Test RMSE:", np.mean(test_rmse_per_target))
 
 
+# Evaluate on test set
+# y_test_pred = model.predict(dtest)
+test_mape_per_target = calculate_mape(y_test, y_test_pred)
+print("\nTest MAPE per target:", test_mape_per_target)
+print("Average Test MAPE:", np.mean(test_mape_per_target))
 
-for i in range(20):
+
+
+for i in range(5):
     plt.figure()
     plt.plot(np.arange(0, len(y_test_pred[i]), 1), y_test[i], 'r-')
     plt.plot(np.arange(0, len(y_test_pred[i]), 1), y_test_pred[i], 'g-')
+    # plt.ylim(0, 1)
 
 
 
