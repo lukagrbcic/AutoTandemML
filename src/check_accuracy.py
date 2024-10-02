e10d3fead6e7c4d0f35ae05f7d2957758782c2bd
lukagrbcic <luka.grbcic@gmail.com>
2024-10-02 14:35:58 -0700

    cleanup and accuracy check update

--- src/check_accuracy.py
+++ src/check_accuracy.py
@@ -1,11 +1,15 @@
 import numpy as np
-from sklearn.metrics import mean_absolute_percentage_error
-
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score
 
 """
 https://www.cirm-math.fr/ProgWeebly/Renc1762/Welch.pdf
 
 source for worst error 
+
+
+https://wires.onlinelibrary.wiley.com/doi/pdfdirect/10.1002/widm.1157
+
+
 """
 
 class error:
@@ -14,44 +18,67 @@ class error:
         
         self.model = model
         self.test_data = test_data #(inputs, outputs)
+        
+        test_inputs, test_outputs = self.test_data        
+
+        
+    @staticmethod
+    def rmse(y_true, y_pred):
+        
+        return np.sqrt(mean_squared_error(y_true, y_pred))
     
     @staticmethod
-    def normalized_rmse(y_true, y_pred):
-        mse = np.mean((y_true - y_pred)**2, axis=0)
-        rmse = np.sqrt(mse)
-        range_y = np.max(y_true, axis=0) - np.min(y_true, axis=0)
-        nrmse = rmse / range_y
-        return np.mean(nrmse)
-
-    @staticmethod
-    def max_normalized_rmse(y_true, y_pred):
-        mse = np.mean((y_true - y_pred)**2, axis=0)
-        rmse = np.sqrt(mse)
-        range_y = np.max(y_true, axis=0) - np.min(y_true, axis=0)
-        nrmse = rmse / range_y
-        return np.max(nrmse)
-
-    @staticmethod
-    def normalized_max_ae(y_true, y_pred):
-        abs_diff = np.abs(y_true - y_pred)
-        abs_diff_mean = np.abs(y_true - np.mean(y_true))
-        nae = np.max(abs_diff)/np.max(abs_diff_mean)
-        return nae
+    def range_nrmse(y_true, y_pred):
+        
+        rmse = np.sqrt(np.mean((y_true - y_pred) ** 2, axis=0))
+        range_ = np.ptp(y_true, axis=0)
+        normalized_rmse = np.mean(rmse/range_)
+        
+        return normalized_rmse
     
-    @staticmethod  
-    def mape(y_true, y_pred):
-        val = np.mean(np.array([mean_absolute_percentage_error(y_true[i], y_pred[i]) for i in range(len(y_pred))]))
-        return val
-           
+    @staticmethod
+    def std_nrmse(y_true, y_pred):
+        
+        rmse = np.sqrt(np.mean((y_true - y_pred) ** 2, axis=0))
+        std = np.std(y_true, axis=0)
+        normalized_rmse = np.mean(rmse/std)
+        
+        return normalized_rmse
+    
+    @staticmethod        
+    def r2(y_true, y_pred):
+        
+        r2 = r2_score(y_true, y_pred, multioutput='raw_values')
+        return np.mean(r2)
+    
+    @staticmethod        
+    def max_rmse(y_true, y_pred):
+        
+        max_rmse_ = np.sqrt(mean_squared_error(y_true, y_pred, multioutput='raw_values'))
+        
+        return max_rmse_
+    
+    @staticmethod
+    def nmax_ae(y_true, y_pred):
+        
+        nmaxae = np.max(np.abs(y_true - y_pred), axis=0)/np.max(np.abs(y_true - np.mean(y_true, axis=0)))
+        
+        return nmaxae
+        
+
     def test_set(self):
         
         test_inputs, test_outputs = self.test_data        
         test_predictions = self.model.predict(test_inputs)
         
-        nrmse = self.normalized_rmse(test_outputs, test_predictions)
-        max_nrmse = self.max_normalized_rmse(test_outputs, test_predictions)
-        max_nae = self.normalized_max_ae(test_outputs, test_predictions)
-        mape_ = self.mape(test_outputs, test_predictions)
 
-        return nrmse, max_nrmse, max_nae, mape_
+        rmse_ = self.rmse(test_outputs, test_predictions)
+        range_nrmse_ = self.range_nrmse(test_outputs, test_predictions)
+        std_nrmse_ = self.std_nrmse(test_outputs, test_predictions)
+        max_rmse_ = self.max_rmse(test_outputs, test_predictions)
+        r2_ = self.r2(test_outputs, test_predictions)
+        nmax_ae_ = self.nmax_ae(test_outputs, test_predictions)
+
+        
+        return rmse_, range_nrmse_, std_nrmse_, max_rmse_, r2_, nmax_ae_
         
