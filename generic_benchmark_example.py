3ff557ca628e691421953770d75d9993c808c77e
lukagrbcic <luka.grbcic@gmail.com>
2024-10-03 08:28:05 -0700

    scalar diffusion case fixed

--- generic_benchmark_example.py
+++ generic_benchmark_example.py
@@ -7,14 +7,13 @@ sys.path.insert(0, '../InverseBench/src/')
 sys.path.insert(1, 'src')
 sys.path.insert(2, 'src/samplers')
 
-
 from benchmarks import *
 import active_learning as al
 
-bench = 'friedman'
-name = 'friedman_multioutput_benchmark'
-# model = load_model(name).load()
-f = benchmark_functions(name)#), model)
+bench = 'scalar_diffusion'
+name = 'scalar_diffusion_benchmark'
+model = load_model(name).load()
+f = benchmark_functions(name, model)
 lb, ub = f.get_bounds()
 
 
@@ -33,16 +32,16 @@ test_data = (test_input, test_output)
 #                              init_size, 
                              # test_data)
                              
-init_size=5
-batch_size=10
-max_samples=750
-sampler='unc_lhs_pso'
-algorithm = ('rf', RandomForestRegressor(n_estimators=68))
+init_size=10
+batch_size=5
+max_samples=100
+sampler='unc_hc'
+algorithm = ('rf', RandomForestRegressor(n_estimators=120))
 
 run = al.activeLearner(f, lb, ub,
                        init_size, batch_size,
                        max_samples, sampler,
                        algorithm,
-                       test_data, verbose=10)
+                       test_data, verbose=batch_size)
 
 run.run()
