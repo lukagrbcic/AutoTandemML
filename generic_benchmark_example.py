c8f402da0b6623defa5ff9c5c75de9b87d7d8552
lukagrbcic <luka.grbcic@gmail.com>
2024-10-10 13:36:59 -0700

    testing airfoils AL

--- generic_benchmark_example.py
+++ generic_benchmark_example.py
@@ -19,11 +19,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-bench = 'friedman'
-name = 'friedman_multioutput_benchmark'
-# model = load_model(name).load()
-# f = benchmark_functions(name, model)
-f = benchmark_functions(name)
+bench = 'airfoils'
+name = 'airfoil_benchmark'
+model = load_model(name).load()
+f = benchmark_functions(name, model)
+# f = benchmark_functions(name)
 
 lb, ub = f.get_bounds()
 
