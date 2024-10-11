302c470e2c8e9e98dffcbfb961192b96c5132c88
lukagrbcic <luka.grbcic@gmail.com>
2024-10-11 08:30:02 -0700

    ensemble for friedman

--- generic_benchmark_example.py
+++ generic_benchmark_example.py
@@ -19,11 +19,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-bench = 'airfoils'
-name = 'airfoil_benchmark'
-model = load_model(name).load()
-f = benchmark_functions(name, model)
-# f = benchmark_functions(name)
+bench = 'friedman'
+name = 'friedman_multioutput_benchmark'
+# model = load_model(name).load()
+# f = benchmark_functions(name, model)
+f = benchmark_functions(name)
 
 lb, ub = f.get_bounds()
 
@@ -38,16 +38,14 @@ n_repeats=30
 sampler='model_entropy'
 # sampler='ensemble'
 # sampler='modelLHS_entropy'
-# sampler='modelHC_quantile'
+# sampler='modelHC_entropy'
 
-# algorithm = ('rf2', RandomForestRegressor())
+# algorithm = ('rf', RandomForestRegressor())
 
 # ensemble = [XGBRegressor(n_estimators=i[1], reg_lambda=i[0]) for i in [[0.1, 10], [0.5,50], [0.8, 75], [1,100], [10, 125]]]             
 # ensemble = []
 # for i in range(3):
 #     ensemble.append(XGBRegressor(n_estimators=np.random.randint(10, 250), reg_lambda=np.random.uniform(0.01, 10)))
-
-
 # algorithm = ('xgb', EnsembleRegressor(ensemble))           
 
 from sklearn.neural_network import MLPRegressor
@@ -59,29 +57,41 @@ for i in range(20):
     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
                                                                   random_state=random.randint(10, 250))))
 
-# for i in range(50):
-#     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(50, 100, 50, 20), 
-#                                                                   random_state=random.randint(10, 250))))
+
 
 algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
-# 
-# ensemble = []
-# for i in range(20):
-#     ensemble.append(RandomForestRegressor(n_estimators=random.randint(10, 150)))#, reg_lambda=np.random.uniform(0.01, 10)))
 
-
-# algorithm = ('rf_ensemble_20', EnsembleRegressor(ensemble))           
-           
-# ensemble = [RandomForestRegressor(n_estimators=i) for i in [50, 80, 100, 250]]             
-# algorithm = ('rfensemble', EnsembleRegressor(ensemble))
 
 results = []
 run = al.activeLearner(f, lb, ub,
                         init_size, batch_size,
                         max_samples, sampler,
                         algorithm,
-                        test_data, verbose=1,initial_hyperparameter_search=True)
+                        test_data, initial_hyperparameter_search=True)
 
+
+file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
+
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        results_exp2 = np.load(file_path, allow_pickle=True).item()
+
+else:
+    print("File does not exist, continuing.")
+
+    results_exp2 = run.run(n_repeats)
+    np.save(file_path, results_exp2)
+
+
+results.append(results_exp2)
+
+
+sampler='ensemble'
+run = al.activeLearner(f, lb, ub,
+                        init_size, batch_size,
+                        max_samples, sampler,
+                        algorithm,
+                        test_data, initial_hyperparameter_search=True)
 
 file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
 
@@ -98,12 +108,15 @@ else:
 
 results.append(results_exp)
 
+
+
+
 sampler='random'
 run = al.activeLearner(f, lb, ub,
                         init_size, batch_size,
                         max_samples, sampler,
                         algorithm,
-                        test_data, verbose=batch_size, initial_hyperparameter_search=True)
+                        test_data, initial_hyperparameter_search=True)
 
 file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
 
