e2232c53a1da5edbaaa4a018e44ec85f9a7e2fb2
lukagrbcic <luka.grbcic@gmail.com>
2024-10-19 10:03:38 -0700

    testing friedman

--- generic_benchmark_example.py
+++ generic_benchmark_example.py
@@ -19,11 +19,11 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-bench = 'inconel'
-name = 'inconel_benchmark'
-model = load_model(name).load()
-f = benchmark_functions(name, model)
-# f = benchmark_functions(name)
+bench = 'friedman'
+name = 'friedman_multioutput_benchmark'
+# model = load_model(name).load()
+# f = benchmark_functions(name, model)
+f = benchmark_functions(name)
 
 lb, ub = f.get_bounds()
 
@@ -31,13 +31,13 @@ test_input = np.load(f'../InverseBench/test_data/{bench}_data/input_test_data.np
 test_output = np.load(f'../InverseBench/test_data/{bench}_data/output_test_data.npy')
 test_data = (test_input, test_output)
 
-init_size=196
-batch_size=196
-max_samples=5*196
+init_size=20
+batch_size=10
+max_samples=200
 n_repeats=1
 sampler='model_entropy'
 
-algorithm = ('rf', RandomForestRegressor())
+# algorithm = ('rf', RandomForestRegressor())
 # ensemble = [XGBRegressor(n_estimators=i[1], reg_lambda=i[0]) for i in [[0.1, 10], [0.5,50], [0.8, 75], [1,100], [10, 125]]]             
 # ensemble = []
 # for i in range(3):
@@ -48,11 +48,11 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-# ensemble = []
-# for i in range(20):
-#     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
-#                                                                   random_state=random.randint(10, 250))))
-# algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
+ensemble = []
+for i in range(20):
+    ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
+                                                                  random_state=random.randint(10, 250))))
+algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
 
 results = []
 run = al.activeLearner(f, lb, ub,
