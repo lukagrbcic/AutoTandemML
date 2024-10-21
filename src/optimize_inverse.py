5c6d450b2209ab6c31914dd425aea3d5897096a1
lukagrbcic <luka.grbcic@gmail.com>
2024-10-21 11:15:18 -0700

    optimize inverse cleanup

--- src/optimize_inverse.py
+++ src/optimize_inverse.py
@@ -6,25 +6,6 @@ import sklearn.metrics
 import numpy as np
 from DNNRegressor import TorchDNNRegressor
 
-param_dist = {
-    'model_type': ['mlp'],
-    'hidden_layers': [[64], [128], [256], [128, 128],
-                      [256, 256], [512, 512], [64, 128, 64],
-                      [128, 256, 128], [256, 512, 256], [64, 128, 256, 128, 64]],
-    'dropout': [0.0, 0.2],
-    'batch_norm': [False, True],
-    'activation': ['relu', 'leaky_relu'],
-    'epochs': [100, 200, 300, 1000],
-    'batch_size': [32, 64],
-    'learning_rate': [0.001, 0.01, 0.1],
-    'input_scaler': [None, 'MinMax', 'Standard'],
-    'output_scaler': [None, 'MinMax', 'Standard'],
-    'output_activation': [None]
-}
-
-material = 'airfoil_Re_3_6'
-X_train = np.load(f'../data/{material}_data/input_train_data.npy')#[:3000]
-y_train = np.load(f'../data/{material}_data/output_train_data.npy')#[:3000]
 
 class get_hyperparameters:
     def __init__(self, X, y, param_dist, n_iter=100, cv=3, 
@@ -46,8 +27,8 @@ class get_hyperparameters:
         
     def run(self):
         
-        model = TorchDNNRegressor(input_size=np.shape(X_train)[1],
-                                  output_size=np.shape(y_train)[1], 
+        model = TorchDNNRegressor(input_size=np.shape(self.X)[1],
+                                  output_size=np.shape(self.y)[1], 
                                   verbose=False, criterion=self.criterion, 
                                   forward_model=self.forward_model)
         
@@ -55,11 +36,10 @@ class get_hyperparameters:
                                             n_iter=self.n_iter, cv=self.cv, verbose=self.verbose, random_state=self.seed, 
                                             scoring=self.scoring, n_jobs=self.n_jobs)
         
-        random_search.fit(X_train, y_train)
+        random_search.fit(self.X, self.y)
         best_params = random_search.best_params_
         
         print("Best parameters found: ", best_params)
 
         return best_params
 
-hyperparams = get_hyperparameters(X_train, y_train, param_dist).run()
