f993d632e016e479dae7444641c0e448f1134cb3
lukagrbcic <luka.grbcic@gmail.com>
2024-10-25 14:44:35 -0700

    three benchmark

--- generic_benchmark_example.py
+++ generic_benchmark_example.py
@@ -19,40 +19,49 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
-bench = 'airfoils'
-name = 'airfoil_benchmark'
-model = load_model(name).load()
-f = benchmark_functions(name, model)
-# f = benchmark_functions(name)
+bench = 'friedman'
+name = 'friedman_multioutput_benchmark'
+f = benchmark_functions(name)
+
+# bench = 'inconel'
+# name = 'inconel_benchmark'
+# model = load_model(name).load()
+# f = benchmark_functions(name, model)
+
+
+# bench = 'airfoils'
+# name = 'airfoil_benchmark'
+# model = load_model(name).load()
+# f = benchmark_functions(name, model)
 
 lb, ub = f.get_bounds()
-
 test_input = np.load(f'../InverseBench/test_data/{bench}_data/input_test_data.npy')
 test_output = np.load(f'../InverseBench/test_data/{bench}_data/output_test_data.npy')
 test_data = (test_input, test_output)
 
-init_size=50
-batch_size=50
-max_samples=450
-n_repeats=10
-sampler='model_entropy'
+init_size=20
+batch_size=10
+max_samples=200
+n_repeats=5
+sampler='model_uncertainty'
+# np.random.seed(43)
 
-algorithm = ('rf', RandomForestRegressor())
+# algorithm = ('rf', RandomForestRegressor())
 # ensemble = [XGBRegressor(n_estimators=i[1], reg_lambda=i[0]) for i in [[0.1, 10], [0.5,50], [0.8, 75], [1,100], [10, 125]]]             
-ensemble = []
-for i in range(3):
-    ensemble.append(XGBRegressor(n_estimators=np.random.randint(10, 250), reg_lambda=np.random.uniform(0.01, 10)))
-algorithm = ('xgb', EnsembleRegressor(ensemble))           
+# ensemble = []
+# for i in range(3):
+#     ensemble.append(XGBRegressor(n_estimators=np.random.randint(10, 250), reg_lambda=np.random.uniform(0.01, 10)))
+# algorithm = ('xgb', EnsembleRegressor(ensemble))           
 
 from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
-# ensemble = []
-# for i in range(20):
-#     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
-#                                                                   random_state=random.randint(10, 250))))
-# algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
+ensemble = []
+for i in range(20):
+    ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
+                                                                  random_state=random.randint(10, 250))))
+algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
 
 results = []
 run = al.activeLearner(f, lb, ub,
@@ -78,30 +87,7 @@ else:
 results.append(results_exp2)
 
 
-sampler='ensemble_cluster'
-run = al.activeLearner(f, lb, ub,
-                        init_size, batch_size,
-                        max_samples, sampler,
-                        algorithm,
-                        test_data)#, initial_hyperparameter_search=True)
-
-file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
-
-if os.path.exists(file_path):
-    with open(file_path, 'r') as file:
-        results_exp = np.load(file_path, allow_pickle=True).item()
-
-else:
-    print("File does not exist, continuing.")
-
-    results_exp = run.run(n_repeats)
-    np.save(file_path, results_exp)
-
-
-results.append(results_exp)
-
-
-# sampler='ensemble_cluster'
+# sampler='model_entropy'
 # run = al.activeLearner(f, lb, ub,
 #                         init_size, batch_size,
 #                         max_samples, sampler,
@@ -112,7 +98,7 @@ results.append(results_exp)
 
 # if os.path.exists(file_path):
 #     with open(file_path, 'r') as file:
-#         results_exp = np.load(file_path, allow_pickle=True).item()A
+#         results_exp = np.load(file_path, allow_pickle=True).item()
 
 # else:
 #     print("File does not exist, continuing.")
@@ -124,26 +110,25 @@ results.append(results_exp)
 # results.append(results_exp)
 
 
+sampler='random'
+run = al.activeLearner(f, lb, ub,
+                        init_size, batch_size,
+                        max_samples, sampler,
+                        algorithm,
+                        test_data)#, initial_hyperparameter_search=True)
 
-# sampler='random'
-# run = al.activeLearner(f, lb, ub,
-#                         init_size, batch_size,
-#                         max_samples, sampler,
-#                         algorithm,
-#                         test_data)#, initial_hyperparameter_search=True)
+file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
 
-# file_path = f'./{bench}_results/{sampler}_{max_samples}_{batch_size}_{n_repeats}_{algorithm[0]}.npy'
+if os.path.exists(file_path):
+    with open(file_path, 'r') as file:
+        results_rnd = np.load(file_path, allow_pickle=True).item()
 
-# if os.path.exists(file_path):
-#     with open(file_path, 'r') as file:
-#         results_rnd = np.load(file_path, allow_pickle=True).item()
+else:
+    print("File does not exist, continuing.")
 
-# else:
-#     print("File does not exist, continuing.")
+    results_rnd = run.run(n_repeats)
+    np.save(file_path, results_rnd)
 
-#     results_rnd = run.run(n_repeats)
-#     np.save(file_path, results_rnd)
-
-# results.append(results_rnd)
+results.append(results_rnd)
 
 pp.plot_results(results).compare_metrics(fname=bench)
