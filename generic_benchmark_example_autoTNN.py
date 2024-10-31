9a6c940062a880f62e52e3c1b6def1011a711c76
lukagrbcic <luka.grbcic@gmail.com>
2024-10-31 13:28:36 -0700

    neat testing

--- generic_benchmark_example_autoTNN.py
+++ generic_benchmark_example_autoTNN.py
@@ -14,27 +14,36 @@ sys.path.insert(2, 'src/samplers')
 from benchmarks import *
 from auto_tandem import AutoTNN
 from ensemble_regressor import EnsembleRegressor
+from inverse_validator import inverse_model_analysis
 
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
-# bench = 'airfoils'
+# bench = 'airfoils' #(xgb ensemble)
 # name = 'airfoil_benchmark'
 # model = load_model(name).load()
 # f = benchmark_functions(name, model)
+# ensemble = [XGBRegressor(n_estimators=i[1], reg_lambda=i[0]) for i in [[0.1, 10], [0.5,50], [0.8, 75], [1,100], [10, 125]]]             
+# algorithm = ('xgb_ensemble', EnsembleRegressor(ensemble))
 
-
-bench = 'friedman' #(deep ensembles)
-name = 'friedman_multioutput_benchmark'
-model = load_model(name).load()
-f = benchmark_functions(name, model)
-
-# bench = 'inconel' #(random forests)
-# name = 'inconel_benchmark'
+# bench = 'friedman' #(deep ensembles)
+# name = 'friedman_multioutput_benchmark'
 # model = load_model(name).load()
 # f = benchmark_functions(name, model)
+# ensemble = []
+# for i in range(20):
+#     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
+#                                                                   random_state=i)))
+# algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble)) 
+
+
+bench = 'inconel' #(random forests)
+name = 'inconel_benchmark'
+model = load_model(name).load()
+f = benchmark_functions(name, model)
+algorithm = ('rf', RandomForestRegressor())
 
 lb, ub = f.get_bounds()
 
@@ -45,28 +54,15 @@ test_data = (test_input, test_output)
 
 init_size=20
 batch_size=10
-max_samples=50
+max_samples=200
 sampler='model_uncertainty'
 # sampler='random'
-
-# ensemble = [XGBRegressor(n_estimators=i[1], reg_lambda=i[0]) for i in [[0.1, 10], [0.5,50], [0.8, 75], [1,100], [10, 125]]]             
-# algorithm = ('xgb_ensemble', EnsembleRegressor(ensemble))
-
-# algorithm = ('rf', RandomForestRegressor())
-
-ensemble = []
-for i in range(20):
-    ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
-                                                                  random_state=i)))
-algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))           
-             
-run = AutoTNN(f, lb, ub, init_size, batch_size, max_samples, algorithm, test_data, sampler=sampler)
-run.get_inverse_DNN()
-inverse_model_config = np.load('inverseDNN/model_config.npy', allow_pickle=True).item()
-
-
-
-
+     
+for i in range(10):
+    print ('Run', i+1)
+    run = AutoTNN(f, lb, ub, init_size, batch_size, max_samples, algorithm, test_data, sampler=sampler, combinations=30)
+    run.get_inverse_DNN()
+    inverse_model_analysis(test_input, test_output, name, sampler).error_metrics()
 
 
 
