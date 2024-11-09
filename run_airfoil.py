99dcc08d55f5013c9ead73f11e1da10c6ccf6982
lukagrbcic <luka.grbcic@gmail.com>
2024-11-09 11:28:24 -0800

    benchmark runs

--- run_airfoil.py
+++ run_airfoil.py
@@ -2,6 +2,10 @@ import sys
 
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
+import random
 
 
 sys.path.insert(0, '../InverseBench/src/')
@@ -24,6 +28,13 @@ f = benchmark_functions(name, model)
 
 algorithm = ('rf', RandomForestRegressor())
 
+# ensemble = []
+# for i in range(10):
+#     ensemble.append(make_pipeline(StandardScaler(), MLPRegressor(hidden_layer_sizes=(100, 200, 100), 
+#                                                                   random_state=random.randint(10, 250))))
+# algorithm = ('mlp_ensemble', EnsembleRegressor(ensemble))      
+
+
 lb, ub = f.get_bounds()
 
 test_input = np.load(f'../InverseBench/test_data/{bench}_data/input_test_data.npy')[:1000]
@@ -33,52 +44,51 @@ test_data = (test_input, test_output)
 all_results_inverse = []
 all_results_forward = []
 
-
 init_size=20
 batch_size=5
 max_samples=150
 n_runs = 30
-sampler = 'random'
+# sampler = 'random'
 
-scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
-                                test_data, algorithm, f, lb, ub, function_name=name)
+# scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+#                                 test_data, algorithm, f, lb, ub, function_name=name)
 
-file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
-file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+# file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+# file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
 
-if os.path.exists(file_path_inverse):
-    with open(file_path_inverse, 'r') as file:
-        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
-    with open(file_path_forward, 'r') as file:
-        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+# if os.path.exists(file_path_inverse):
+#     with open(file_path_inverse, 'r') as file:
+#         results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+#     with open(file_path_forward, 'r') as file:
+#         results_forward = np.load(file_path_forward, allow_pickle=True).item()
 
-else:
-    results_inverse, results_forward = scalar_setup.run()
+# else:
+#     results_inverse, results_forward = scalar_setup.run()
 
-all_results_inverse.append(results_inverse)
-all_results_forward.append(results_forward)
+# all_results_inverse.append(results_inverse)
+# all_results_forward.append(results_forward)
 
-sampler = 'lhs'
+# sampler = 'lhs'
 
-scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
-                                test_data, algorithm, f, lb, ub, function_name=name)
+# scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+#                                 test_data, algorithm, f, lb, ub, function_name=name)
     
-file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
-file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+# file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+# file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
 
-if os.path.exists(file_path_inverse):
-    with open(file_path_inverse, 'r') as file:
-        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
-    with open(file_path_forward, 'r') as file:
-        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+# if os.path.exists(file_path_inverse):
+#     with open(file_path_inverse, 'r') as file:
+#         results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+#     with open(file_path_forward, 'r') as file:
+#         results_forward = np.load(file_path_forward, allow_pickle=True).item()
 
-else:
-    results_inverse, results_forward = scalar_setup.run()
+# else:
+#     results_inverse, results_forward = scalar_setup.run()
 
-all_results_inverse.append(results_inverse)
-all_results_forward.append(results_forward)
+# all_results_inverse.append(results_inverse)
+# all_results_forward.append(results_forward)
 
-sampler = 'model_uncertainty'
+sampler = 'modelLHS_quantile'
 
 scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
                                 test_data, algorithm, f, lb, ub, function_name=name)
