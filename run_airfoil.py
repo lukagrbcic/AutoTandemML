f2e179f8c27ef54e045096a9cbf6a649611c88de
lukagrbcic <luka.grbcic@gmail.com>
2024-11-11 14:46:14 -0800

    added greedyfp and bc for benchmarks

--- run_airfoil.py
+++ run_airfoil.py
@@ -48,47 +48,7 @@ init_size=20
 batch_size=5
 max_samples=150
 n_runs = 30
-# sampler = 'random'
-
-# scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
-#                                 test_data, algorithm, f, lb, ub, function_name=name)
-
-# file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
-# file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
-
-# if os.path.exists(file_path_inverse):
-#     with open(file_path_inverse, 'r') as file:
-#         results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
-#     with open(file_path_forward, 'r') as file:
-#         results_forward = np.load(file_path_forward, allow_pickle=True).item()
-
-# else:
-#     results_inverse, results_forward = scalar_setup.run()
-
-# all_results_inverse.append(results_inverse)
-# all_results_forward.append(results_forward)
-
-# sampler = 'lhs'
-
-# scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
-#                                 test_data, algorithm, f, lb, ub, function_name=name)
-    
-# file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
-# file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
-
-# if os.path.exists(file_path_inverse):
-#     with open(file_path_inverse, 'r') as file:
-#         results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
-#     with open(file_path_forward, 'r') as file:
-#         results_forward = np.load(file_path_forward, allow_pickle=True).item()
-
-# else:
-#     results_inverse, results_forward = scalar_setup.run()
-
-# all_results_inverse.append(results_inverse)
-# all_results_forward.append(results_forward)
-
-sampler = 'modelLHS_quantile'
+sampler = 'random'
 
 scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
                                 test_data, algorithm, f, lb, ub, function_name=name)
@@ -108,5 +68,86 @@ else:
 all_results_inverse.append(results_inverse)
 all_results_forward.append(results_forward)
 
-plot_results(all_results_inverse).compare_metrics(name,'inverse')
-# plot_results(all_results_forward).compare_metrics(name,'forward')
+sampler = 'lhs'
+
+scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+                                test_data, algorithm, f, lb, ub, function_name=name)
+    
+file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+
+if os.path.exists(file_path_inverse):
+    with open(file_path_inverse, 'r') as file:
+        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+    with open(file_path_forward, 'r') as file:
+        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+
+else:
+    results_inverse, results_forward = scalar_setup.run()
+
+all_results_inverse.append(results_inverse)
+all_results_forward.append(results_forward)
+
+sampler = 'bc'
+
+scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+                                test_data, algorithm, f, lb, ub, function_name=name)
+    
+file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+
+if os.path.exists(file_path_inverse):
+    with open(file_path_inverse, 'r') as file:
+        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+    with open(file_path_forward, 'r') as file:
+        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+
+else:
+    results_inverse, results_forward = scalar_setup.run()
+
+all_results_inverse.append(results_inverse)
+all_results_forward.append(results_forward)
+
+sampler = 'greedyfp'
+
+scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+                                test_data, algorithm, f, lb, ub, function_name=name)
+    
+file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+
+if os.path.exists(file_path_inverse):
+    with open(file_path_inverse, 'r') as file:
+        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+    with open(file_path_forward, 'r') as file:
+        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+
+else:
+    results_inverse, results_forward = scalar_setup.run()
+
+all_results_inverse.append(results_inverse)
+all_results_forward.append(results_forward)
+
+
+sampler = 'model_uncertainty'
+
+scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+                                test_data, algorithm, f, lb, ub, function_name=name)
+
+file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+
+if os.path.exists(file_path_inverse):
+    with open(file_path_inverse, 'r') as file:
+        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+    with open(file_path_forward, 'r') as file:
+        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+
+else:
+    results_inverse, results_forward = scalar_setup.run()
+
+all_results_inverse.append(results_inverse)
+all_results_forward.append(results_forward)
+
+# plot_results(all_results_inverse).compare_metrics(name,'inverse')
+plot_results(all_results_forward).compare_metrics(name,'forward')
