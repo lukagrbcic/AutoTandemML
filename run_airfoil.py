c58d28690a8f280bf149f72bc386a691d6264a79
lukagrbcic <luka.grbcic@gmail.com>
2024-11-13 09:10:39 -0800

    greedy test on airfoil and inconel

--- run_airfoil.py
+++ run_airfoil.py
@@ -48,6 +48,32 @@ init_size=20
 batch_size=5
 max_samples=150
 n_runs = 30
+
+
+
+sampler = 'modelgreedy_uncertainty'
+
+scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+                                test_data, algorithm, f, lb, ub, function_name=name)
+
+file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+
+if os.path.exists(file_path_inverse):
+    with open(file_path_inverse, 'r') as file:
+        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+    with open(file_path_forward, 'r') as file:
+        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+
+else:
+    results_inverse, results_forward = scalar_setup.run()
+
+all_results_inverse.append(results_inverse)
+all_results_forward.append(results_forward)
+
+
+
+
 sampler = 'random'
 
 scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
