6e1f6be115690a36314cd7dd0a1710525f6199ab
lukagrbcic <luka.grbcic@gmail.com>
2024-11-18 11:07:33 -0800

    returned to original setup

--- run_scalar.py
+++ run_scalar.py
@@ -53,30 +53,30 @@ test_data = (test_input, test_output)
 all_results = []
 
 init_size=20
-batch_size=50
+batch_size=5
 max_samples=400
 n_runs = 30
 
 
-sampler = 'modelgreedy_uncertainty'
+# sampler = 'modelgreedy_uncertainty'
 
-scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
-                                test_data, algorithm, f, lb, ub, function_name=name)
+# scalar_setup = experiment_setup(sampler, n_runs, init_size, batch_size, max_samples, 
+#                                 test_data, algorithm, f, lb, ub, function_name=name)
 
-file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
-file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
+# file_path_inverse = f'./{name}_results/inverseDNN_{sampler}_{n_runs}.npy'
+# file_path_forward = f'./{name}_results/forward_model_{sampler}_{n_runs}.npy'
 
-if os.path.exists(file_path_inverse):
-    with open(file_path_inverse, 'r') as file:
-        results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
-    with open(file_path_forward, 'r') as file:
-        results_forward = np.load(file_path_forward, allow_pickle=True).item()
+# if os.path.exists(file_path_inverse):
+#     with open(file_path_inverse, 'r') as file:
+#         results_inverse = np.load(file_path_inverse, allow_pickle=True).item()
+#     with open(file_path_forward, 'r') as file:
+#         results_forward = np.load(file_path_forward, allow_pickle=True).item()
 
-else:
-    results_inverse, results_forward = scalar_setup.run()
+# else:
+#     results_inverse, results_forward = scalar_setup.run()
 
-all_results_inverse.append(results_inverse)
-all_results_forward.append(results_forward)
+# all_results_inverse.append(results_inverse)
+# all_results_forward.append(results_forward)
 
 
 
@@ -181,5 +181,5 @@ else:
 all_results_inverse.append(results_inverse)
 all_results_forward.append(results_forward)
 
-plot_results(all_results_inverse).compare_metrics(name,'inverse')
-# plot_results(all_results_forward).compare_metrics(name,'forward')
+# plot_results(all_results_inverse).compare_metrics(name,'inverse')
+plot_results(all_results_forward).compare_metrics(name,'forward')
